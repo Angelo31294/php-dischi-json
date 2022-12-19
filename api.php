7a3765350203adddf2449c994a88cc31595414e7
Angelo31294 <luongoangelo1@gmail.com>
2022-12-19 10:38:48 +0100

    Correzione

--- api.php
+++ api.php
@@ -1,7 +1,5 @@
 <?php
-$album = file_get_contents("dischi.json");
+$albums = file_get_contents("dischi.json");
 
-$albums = json_decode($album, true);
-$jason_string = json_encode($albums);
 header("Content-Type: application/json");
-echo $json_string;
+echo $albums;
